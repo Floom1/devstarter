d94706c8b50573be63b0afadf46d267abf0193b2
Aleksandr <Aleksandr260206@yandex.ru>
2025-03-31 13:38:47 +0300

    create template model

--- apps/templates_app/admin.py
+++ apps/templates_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
+from .models import Category, Template
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    pass
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
--- apps/templates_app/models.py
+++ apps/templates_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Template(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField
+    file = models.FileField(upload_to='templates/')
+    category = TreeForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
