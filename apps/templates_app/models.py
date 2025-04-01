76949d7736b5224c0487d8adbe80504e32581a93
Aleksandr <Aleksandr260206@yandex.ru>
2025-04-01 13:06:40 +0300

    add manager for templates

--- apps/templates_app/models.py
+++ apps/templates_app/models.py
@@ -15,15 +15,39 @@ class Category(MPTTModel):
     class MPTTMeta:
         order_insertion_by = ['name']
 
+    class Meta:
+        db_table = 'dev_category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     def __str__(self):
         return self.name
+
+
+class TemplateManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
 
 
 class Template(models.Model):
-    name = models.CharField(max_length=70)
-    description = models.TextField
-    file = models.FileField(upload_to='templates/')
-    category = TreeForeignKey(Category, on_delete=models.CASCADE)
+    # STATUS_OPTIONS = (
+    #     ('published', 'Опубликовано'),
+    #     ('draft', 'Черновик')
+    # )
+    name = models.CharField(max_length=70, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', default='')
+    file = models.FileField(upload_to='templates/', verbose_name='Шаблон')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = TreeForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    objects=models.Manager()
+    custom = TemplateManager()
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'dev_template'
+        verbose_name = 'Шаблон'
+        verbose_name_plural = 'Шаблоны'
+        indexes = [models.Index(fields=['is_published'])]
