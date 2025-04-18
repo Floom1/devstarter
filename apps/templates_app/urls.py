7d462567551a7109f94d51442ea6cc8cc884fb64
Aleksandr <Aleksandr260206@yandex.ru>
2025-04-18 16:29:34 +0300

    add a creating repo with users name

--- apps/templates_app/urls.py
+++ apps/templates_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', CategoryListView.as_view(), name='home'),
     path('category/<slug:slug>/', TemplateListView.as_view(), name='category_templates'),
     path('download/<int:id>', download_template, name='download_template'),
+    path('create-repo/<int:template_id>', RepoCreateView.as_view(), name='create-repo'),
 ]
--- apps/templates_app/views.py
+++ apps/templates_app/views.py
@@ -1,3 +1,6 @@
+from django.db import transaction
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Template, Category
@@ -5,6 +8,11 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, FileResponse
 import os
 from django.conf import settings
+from .forms import RepoCreateForm
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 class TemplateListView(ListView):
     model = Template # {% url 'download_template' template.id %} !!!!!
@@ -75,3 +83,84 @@ def download_template(request, id):
             return HttpResponse(f"Файл не найден: {file_path}", status=404)
     except Template.DoesNotExist:
         return HttpResponse("Шаблон не найден", status=404)
+
+
+class RepoCreateView(LoginRequiredMixin, View):
+    template_name = 'templates_app/create_repo.html'
+
+    def get(self, request, template_id):
+            form = RepoCreateForm()
+            return render(request, self.template_name, {
+                'title': 'Добавление названия репозитория',
+                'form': form,
+                'template_id': template_id
+            })
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['title'] = f'Добавление названия репозитория'
+    #     if self.request.POST:
+    #         context['repo-create_form'] = RepoCreateForm(self.request.POST,
+    #                                               instance=self.request.user)
+    #     else:
+    #         context['repo-create_form'] = RepoCreateForm(instance=self.request.user)
+    #     return context
+
+    # def form_valid(self, form):
+    #     context = self.get_context_data()
+    #     repo_create_form = context['repo-create_form']
+    #     with transaction.atomic():
+    #         if all([form.is_valid(), repo_create_form.is_valid()]):
+    #             repo_create_form.save()
+    #             form.save()
+    #         else:
+    #             context.update({"repo-create_form": repo_create_form})
+    #             return self.render_to_response(context)
+    #     return super().form_valid(form)
+
+
+    def post(self, request, template_id):
+        form = RepoCreateForm(request.POST)
+        if form.is_valid():
+            repo_name = form.cleaned_data['repo_name']
+            template = get_object_or_404(Template, id=template_id)
+            token = request.user.profile.github_token
+            owner = request.user.social_auth.get(provider='github').uid
+
+            headers = {
+                "Authorization": f"token {token}",
+                "Accept": "application/vnd.github.v3+json"
+            }
+
+            # Проверка доступности имени
+            check_url = f"https://api.github.com/repos/{owner}/{repo_name}"
+            check_response = requests.get(check_url, headers=headers)
+
+            if check_response.status_code == 200:
+                return render(request, self.template_name, {
+                    'form': form,
+                    'error': 'Имя репозитория уже занято!'
+                })
+            elif check_response.status_code != 404:
+                return render(request, self.template_name, {
+                    'form': form,
+                    'error': 'Ошибка при проверке имени!'
+                })
+
+            # Создание репозитория
+            data = {
+                "name": repo_name,
+                "description": f"Шаблон: {template.description}",
+                "private": False,
+            }
+            response = requests.post(
+                "https://api.github.com/user/repos",
+                headers=headers,
+                json=data
+            )
+
+            if response.status_code == 201:
+                return JsonResponse({"success": True, "repo_url": response.json()["html_url"]})
+            else:
+                return JsonResponse({"error": response.json()["message"]}, status=400)
+        return render(request, self.template_name, {'form': form})
